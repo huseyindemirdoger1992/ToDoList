82dbb319636675ae05ad1bcf5742e953fdcc12ea
Huseyin <38278270+huseyindemirdoger1992@users.noreply.github.com>
2021-06-17 05:51:40 +0300

    Add project files.

---
+++ ToDoList/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Listele()
+        {
+            ToDoListEntitiesConnectionStringDB db = new ToDoListEntitiesConnectionStringDB();
+            ViewBag.Liste = db.Lists;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Kaydet(List l)
+        {
+            ToDoListEntitiesConnectionStringDB db = new ToDoListEntitiesConnectionStringDB();
+            db.Lists.Add(l);
+            db.SaveChanges();
+            return RedirectToAction("Listele");
+        }
+        [HttpPost]
+        public ActionResult Duzenle( List D)
+        {
+            ToDoListEntitiesConnectionStringDB db = new ToDoListEntitiesConnectionStringDB();
+            List KL = db.Lists.FirstOrDefault(x => x.id == D.id);
+            KL.Title = D.Title;
+            KL.Description = D.Description;
+            db.SaveChanges();
+            return RedirectToAction("Listele");
+        }
+
+        public ActionResult Sil(int id)
+        {
+            ToDoListEntitiesConnectionStringDB db = new ToDoListEntitiesConnectionStringDB();
+            List silinecek = db.Lists.FirstOrDefault(x => x.id == id);
+            db.Lists.Remove(silinecek);
+            db.SaveChanges();
+            return RedirectToAction("Listele");
+        }
+    }
+}
